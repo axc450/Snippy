dca217636bfaf2bb3e0a9e23237bd8695c2a7080
axc450 <antoniocantone96@googlemail.com>
2017-09-23 16:38:01 +0100

    Added Trigger

--- Snippy.vb
+++ Snippy.vb
@@ -1,4 +1,25 @@
-﻿Public Class Snippy
+﻿Imports System.Runtime.InteropServices
+
+Public Class Snippy
+    Public Const MOD_ALT As Integer = &H1
+    Public Const WM_HOTKEY As Integer = &H312
+
+    <DllImport("User32.dll")>
+    Public Shared Function RegisterHotKey(ByVal hwnd As IntPtr, ByVal id As Integer, ByVal fsModifiers As Integer, ByVal vk As Integer) As Integer
+    End Function
+
+    Private Sub Snippy_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        RegisterHotKey(Me.Handle, 1, MOD_ALT, Keys.D4)
+    End Sub
+
+    Protected Overrides Sub WndProc(ByRef m As System.Windows.Forms.Message)
+        If m.Msg = WM_HOTKEY Then
+            MsgBox("Alt+4 Pressed!")
+        End If
+
+        MyBase.WndProc(m)
+    End Sub
+
     Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
         Me.Close()
     End Sub
