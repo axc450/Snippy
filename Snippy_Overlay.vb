39af969c1b8e6f1be51178f83402eaff524005dd
axc450 <antoniocantone96@googlemail.com>
2017-09-23 18:26:40 +0100

    Added mouse selection

--- Snippy_Overlay.vb
+++ Snippy_Overlay.vb
@@ -1,9 +1,13 @@
 ﻿Public Class Snippy_Overlay
 
+    Dim isDrawing As Boolean
     Dim beginX As Integer = 0
     Dim beginY As Integer = 0
     Dim endX As Integer = 0
     Dim endY As Integer = 0
+    Dim b As SolidBrush = New SolidBrush(Color.Black)
+    Dim p As Pen = New Pen(Color.Red, 5)
+
 
     Private Sub Snippy_Overlay_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
         If e.KeyCode = Keys.Escape Then
@@ -14,13 +18,40 @@
     Private Sub Sinppy_Overaly_MouseDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles MyBase.MouseDown
         beginX = e.X
         beginY = e.Y
+        isDrawing = True
+    End Sub
+
+    Private Sub Sinppy_Overaly_MouseMove(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles MyBase.MouseMove
+        If (isDrawing) Then
+            endX = e.X
+            endY = e.Y
+            Me.Invalidate()
+        End If
     End Sub
 
     Private Sub Sinppy_Overaly_MouseUp(ByVal sender As System.Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles MyBase.MouseUp
-        endX = e.X
-        endY = e.Y
-        MsgBox(beginX & " " & beginY & " " & endX & " " & endY)
+        isDrawing = False
         Me.Close()
     End Sub
 
+    Private Sub Sinppy_Overaly_Paint(ByVal sender As Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Me.Paint
+
+        Dim X As Integer = beginX
+        Dim Y As Integer = beginY
+        Dim W As Integer = endX - beginX
+        Dim H As Integer = endY - beginY
+
+        If (endX < beginX) Then
+            X = endX
+            W = beginX - endX
+        End If
+
+        If (endY < beginY) Then
+            Y = endY
+            H = beginY - endY
+        End If
+
+        e.Graphics.FillRectangle(b, X, Y, W, H)
+        e.Graphics.DrawRectangle(p, X, Y, W, H)
+    End Sub
 End Class
